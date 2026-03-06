2c0397a4c762b403713d81c2f5331461630726f5
habinezajeanbosco4114-tech <habinezajeanbosco4114@gmail.com>
2026-03-06 06:54:24 +0200

    Update script.js
    Signed-off-by: habinezajeanbosco4114-tech <habinezajeanbosco4114@gmail.com>

--- script.js
+++ script.js
@@ -5,21 +5,23 @@ let url = document.getElementById("videoUrl").value;
 let message = document.getElementById("message");
 
 if(url === ""){
-message.innerText="Please paste a video link.";
+
+message.innerText="Please paste a YouTube link.";
+
 return;
+
 }
 
 if(url.includes("youtube.com") || url.includes("youtu.be")){
-message.innerText="Processing YouTube link...";
-window.open("https://y2mate.is/en/youtube-downloader/?url="+url);
+
+message.innerText="Preparing download...";
+
+window.location.href="https://y2mate.is/";
+
 return;
+
 }
 
-if(url.includes("instagram.com")){
-message.innerText="Processing Instagram link...";
-window.open("https://igram.world/?url="+url);
-return;
-}
+message.innerText="Only YouTube links supported.";
 
-message.innerText="Unsupported link. Use YouTube or Instagram.";
 }
